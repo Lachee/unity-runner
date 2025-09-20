a79370a56cf34fa4bb7cecc07441a83dad10c068
Lake <lake.hopkins@outlook.com>
2025-09-20 12:55:40 +1000

    tweaked build ci/cd

--- .gitea/workflows/scripts/build-runner-image.sh
+++ .gitea/workflows/scripts/build-runner-image.sh
@@ -11,16 +11,16 @@ fi
 
 # Fetch the changelog and extract the changeset ID for the specified Unity version
 # This is because hub doesnt remember every version of unity and uses the changset for the exact id lookup.
-if [ -z "${UNITY_CHANGESET}" ]; then
-    echo "Warning: No changeset provided. Scraping one from the change logs."
-    echo "This might take a while. Use the UNITY_CHANGESET to avoid this lookup."
-    CHANGELOG_URL="https://unity.com/releases/editor/whats-new/${UNITY_VERSION}"
-    UNITY_CHANGESET=$(curl -s -r 0-500 "$CHANGELOG_URL" | grep -oP 'unityhub://(?:[0-9a-z.])+/\K([a-z0-9]+)' | head -n 1)
-    if [ -z "$UNITY_CHANGESET" ]; then
-        echo "Error: Could not extract changeset for Unity version ${UNITY_VERSION}."
-        exit 1
-    fi
-fi
+# if [ -z "${UNITY_CHANGESET}" ]; then
+#     echo "Warning: No changeset provided. Scraping one from the change logs."
+#     echo "This might take a while. Use the UNITY_CHANGESET to avoid this lookup."
+#     CHANGELOG_URL="https://unity.com/releases/editor/whats-new/${UNITY_VERSION}"
+#     UNITY_CHANGESET=$(curl -s -r 0-500 "$CHANGELOG_URL" | grep -oP 'unityhub://(?:[0-9a-z.])+/\K([a-z0-9]+)' | head -n 1)
+#     if [ -z "$UNITY_CHANGESET" ]; then
+#         echo "Error: Could not extract changeset for Unity version ${UNITY_VERSION}."
+#         exit 1
+#     fi
+# fi
 
 # Ensure we have some modules
 if [ -z "${UNITY_MODULES}" ]; then
@@ -28,16 +28,6 @@ if [ -z "${UNITY_MODULES}" ]; then
         UNITY_MODULES=$3
     else
         echo "Error: UNITY_MODULES is not set."
-        exit 1
-    fi
-fi
-
-# Ensure IMAGE is set, pull from arguments if not
-if [ -z "${IMAGE}" ]; then
-    if [ -n "$3" ]; then
-        IMAGE=$3
-    else
-        echo "Error: IMAGE is not set."
         exit 1
     fi
 fi
@@ -74,6 +64,22 @@ if [ -z "${PLATFORM}" ]; then
     esac
 fi
 
+# Ensure IMAGE is set, pull from arguments if not
+if [ -z "${IMAGE}" ]; then
+    if [ -n "$3" ]; then
+        IMAGE=$3
+    else
+        echo "Error: IMAGE is not set."
+        exit 1
+    fi
+fi
+
+# Ensure the TAG is set, default to latest if not
+if [ -z "${TAG}" ]; then
+    TAG="${GAMECI_OS}-${UNITY_VERSION}-runner"
+fi
+
+
 # Ensure some additional build settings are set
 if [ -z "${DOCKER_BUILD_ARGS}" ]; then
     DOCKER_BUILD_ARGS=""
@@ -81,21 +87,19 @@ fi
 
 BASE_TAG=${GAMECI_OS}-${UNITY_VERSION}-base-${GAMECI_VERSION}
 BASE_IMAGE=unityci/editor:${BASE_TAG}
-DEST_TAG=${GAMECI_OS}-${UNITY_VERSION}-runner
-DEST_IMAGE=${IMAGE}:${DEST_TAG}
+DEST_IMAGE=${IMAGE}:${TAG}
 
 echo "Building Docker image ${DEST_IMAGE}"
 echo "- Version: ${UNITY_VERSION}"
 echo "- Changeset: ${UNITY_CHANGESET}"
 echo "- Platfrom: ${PLATFORM}"
 echo "- Base: ${BASE_IMAGE}"
-echo "- Tag: ${DEST_TAG}"
+echo "- Tag: ${TAG}"
 echo "- Image: ${DEST_IMAGE}"
 
-docker buildx build \
+docker build \
     --platform ${PLATFORM} \
     --build-arg "VERSION=${UNITY_VERSION}" \
-    --build-arg "CHANGESET=${UNITY_CHANGESET}" \
     --build-arg "BASE_IMAGE=${BASE_IMAGE}" \
     --build-arg "MODULE=${UNITY_MODULES}" \
     -t ${DEST_IMAGE} ${DOCKER_BUILD_ARGS} \
@@ -109,6 +113,6 @@ fi
 # Export IMAGE and TAG for GitHub Actions
 if [ -n "$GITHUB_OUTPUT" ]; then
     echo "IMAGE=${IMAGE}" >> $GITHUB_OUTPUT
-    echo "TAG=$DEST_TAG" >> $GITHUB_OUTPUT
+    echo "TAG=$TAG" >> $GITHUB_OUTPUT
     echo "FULL_IMAGE=${DEST_IMAGE}" >> $GITHUB_OUTPUT
 fi
